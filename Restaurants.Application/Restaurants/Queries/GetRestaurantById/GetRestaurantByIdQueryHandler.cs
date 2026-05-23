f65cbc06b478046265c88eb3f0b8eeb86513eafa
expanse256 <marcindotmentel@gmail.com>
2026-05-23 13:31:57 +0200

    imporve

--- Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
+++ Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
@@ -16,7 +16,7 @@ public class GetRestaurantByIdQueryHandler(ILogger<GetRestaurantByIdQueryHandler
 {
     public async Task<RestaurantDto> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Getting restaurant {RestaurantId}", request.Id);
+        logger.LogInformation("Getting restaurant:{RestaurantId}", request.Id);
         var restaurant = await restaurantsRepository.GetByIdAsync(request.Id) ?? throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
 
         var restaurantDto = mapper.Map<RestaurantDto>(restaurant);
